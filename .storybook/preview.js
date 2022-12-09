37f772b2778fa1c733808a816ed10630cc93900f
Michelle Miller <chellimiller@users.noreply.github.com>
2022-12-09 15:52:35 -0700

    Add Sass support for Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,16 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  options: {
+    storySort: {
+      method: 'alphabetical',
+      order: ['Theme', 'Components'],
+      locales: 'en-US',
+    },
+  },
+};
