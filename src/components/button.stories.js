1d9c29df225b073ee53a5e549b395c0c8283b182
Michelle Miller <chellimiller@users.noreply.github.com>
2022-12-09 18:24:35 -0700

    Move button and add stories

---
+++ src/components/button.stories.js
@@ -0,0 +1,23 @@
+import { generateHtml, createArgTypes } from '../_storybook/util';
+import '../theme/tokens.scss';
+import './button.scss';
+
+export default {
+  title: 'components/Button',
+  argTypes: createArgTypes({
+    tag: 'button',
+    styleArgTypes: {
+      variant: {
+        options: ['dx-button--variant-outlined'],
+        description:
+          'This class is optional when using a button or input with the type "button", "submit", or "reset"',
+      },
+    },
+  }),
+};
+
+export const Button = (args) => generateHtml(args);
+
+Button.parameters = {
+  docs: { source: { type: 'dynamic' } },
+};
